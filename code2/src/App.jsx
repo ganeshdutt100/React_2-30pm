217dda5c35173a4379341a95e0b27fcc11b43b18
unknown <gdutt2304@gmail.com>
2024-10-22 15:56:59 +0530

    first commit

---
+++ code2/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  let a = (
+    <img
+      src="https://cdn.pixabay.com/photo/2024/06/14/12/15/developer-8829735_1280.jpg"
+      alt=""
+    />
+  );
+  return (
+    <>
+      <h1>hello world</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, eos.
+          Beatae ipsam sed amet iure voluptatum ab ipsum, deserunt, minima,
+          expedita quos eum voluptatibus. Quis numquam ducimus sit non eligendi!
+        </p>
+        <p>{a}</p>
+        <p>{a}</p>
+        <p>{a}</p>
+        <p>{a}</p>
+        <p>{a}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
